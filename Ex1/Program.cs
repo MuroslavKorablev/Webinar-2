2b2088a494192cbef32f40ef77a449297f3a5f9d
Kleo <mmttdd16@gmail.com>
2023-03-26 10:24:59 +0300

    Create Ex2

--- Ex1/Program.cs
+++ Ex1/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Random rnd = new Random();
+int number = rnd.Next(10,99);
+
+int firstNumber = number / 10;
+int lastnNumber = number % 10;
+int MaxNumber = Math.Max(firstNumber, lastnNumber);
+
+System.Console.WriteLine(number);
+System.Console.WriteLine(MaxNumber);
