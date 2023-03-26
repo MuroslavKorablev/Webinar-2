e7c1c05c67f8f88a198a3043ef9801acdeddc20a
Kleo <mmttdd16@gmail.com>
2023-03-26 15:54:43 +0300

    Finished Ex6

--- Ex6/Program.cs
+++ Ex6/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int n = Convert.ToInt32(System.Console.ReadLine());
+float sum = 0;
+
+for (int i = 1; i <= n; i++)
+{
+    sum += (float) 1/(float)i;
+    System.Console.WriteLine((float) 1/(float)i);
+}
+System.Console.WriteLine(sum);
