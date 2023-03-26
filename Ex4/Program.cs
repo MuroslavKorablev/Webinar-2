02b1db25dcd996f51eab17626f6e85951449d21a
Kleo <mmttdd16@gmail.com>
2023-03-26 12:22:55 +0300

    Create Ex5

--- Ex4/Program.cs
+++ Ex4/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿System.Console.WriteLine("Введите число");
+int Number = Convert.ToInt32(System.Console.ReadLine());
+int b = Number % 7;
+int c = Number % 23;
+
+if(b == 0 && c == 0)
+    System.Console.WriteLine("кратно");
+else
+    System.Console.WriteLine("Не кратно");
+System.Console.WriteLine(b + c);
