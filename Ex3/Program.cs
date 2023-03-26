9f674d56c9a1d65981ecc62c93983b12af8e304e
Kleo <mmttdd16@gmail.com>
2023-03-26 11:46:54 +0300

    Create Ex4

--- Ex3/Program.cs
+++ Ex3/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿System.Console.WriteLine("Введите первое число");
+int firstNumber = Convert.ToInt32(System.Console.ReadLine());
+System.Console.WriteLine("Введите второе число");
+int secondNumber = Convert.ToInt32(System.Console.ReadLine());
+
+if(firstNumber % secondNumber == 0)
+    System.Console.WriteLine("кратно");
+else
+    System.Console.WriteLine($"{firstNumber} и {secondNumber} не кратно = {firstNumber % secondNumber}");
+
