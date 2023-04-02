0377fee54b84556e008e617ff695a25b06d39e5e
Kleo <mmttdd16@gmail.com>
2023-04-02 11:52:20 +0300

    Finished Ex7

--- Ex7/Program.cs
+++ Ex7/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Вычислите сумму от 2^0 до 2^10
+int number = 2;
+int sum = 0;
+for (int i = 0; i <=10; i++)
+{
+    System.Console.WriteLine($"{"индекс ="} {i}");
+    sum += (int)Math.Pow(number , i);
+    System.Console.WriteLine($"{"сумма ="} {sum}");  
+}
